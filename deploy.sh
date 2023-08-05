b1def72ef046f6812d1f028df6623e629decf082
DenisRegan <denismartinoregan@hotmail.com>
2023-08-05 08:27:10 +0100

    version

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #! /usr/bin/env bash
 sudo apt update && sudo apt install nodejs npm
+sudo apt install ruby-full sqlite3
+sudo gem install rails
+sudo apt install ruby bundler
 #install pm2
 sudo npm install -g pm2
 #stop any instance of the application running
